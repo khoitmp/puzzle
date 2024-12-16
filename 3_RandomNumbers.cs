ad7814e197a58bbd6f66bcb2f1dd2521e453fcd0
khoitmp <khoitmp>
2024-12-16 08:46:01 +0700

    Add RandomNumbers problem

---
+++ 3_RandomNumbers.cs
@@ -0,0 +1,21 @@
+var nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var processed = 0;
+
+GetRandom();
+GetRandom();
+GetRandom();
+GetRandom();
+GetRandom();
+GetRandom();
+GetRandom();
+GetRandom();
+GetRandom();
+GetRandom();
+
+/* 
+    TODO: Implement here...
+*/
+void GetRandom()
+{
+    throw new NotImplementedException();
+}
